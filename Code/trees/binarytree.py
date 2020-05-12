c2deb795c8187ab2b85cc11eacc2c5c7097075f2
Cao Mai <caomai@rocketmail.com>
2020-05-12 07:41:12 -0700

    [add] insert method to binarytree

--- Code/trees/binarytree.py
+++ Code/trees/binarytree.py
@@ -15,12 +15,12 @@ class BinaryTreeNode(object):
 
     def is_leaf(self):
         """Return True if this node is a leaf (has no children)."""
-        # TODO: Check if both left child and right child have no value
+        # Check if both left child and right child have no value
         return self.left is None and self.right is None
 
     def is_branch(self):
         """Return True if this node is a branch (has at least one child)."""
-        # TODO: Check if either left child or right child has a value
+        # Check if either left child or right child has a value
         return self.left is not None or self.right is not None
 
     def height(self):
@@ -33,10 +33,10 @@ class BinaryTreeNode(object):
 
         left_height = 0
         right_height = 0
-        # TODO: Check if left child has a value and if so calculate its height
+        # Check if left child has a value and if so calculate its height
         if self.left is not None:
             left_height = self.left.height() + 1
-        # TODO: Check if right child has a value and if so calculate its height
+        # Check if right child has a value and if so calculate its height
         if self.right is not None:
             right_height = self.right.height() + 1
         # Return one more than the greater of the left height and right height
@@ -64,14 +64,17 @@ class BinarySearchTree(object):
     def height(self):
         """Return the height of this tree (the number of edges on the longest
         downward path from this tree's root node to a descendant leaf node).
-        TODO: Best and worst case running time: ??? under what conditions?"""
-        # TODO: Check if root node has a value and if so calculate its height
-        ...
+        Best and worst case running time: O(h) where h is the height of the tree"""
+        # Check if root node has a value and if so calculate its height
+        if self.root is not None:
+            return self.root.height()
+        return 0
 
     def contains(self, item):
         """Return True if this binary search tree contains the given item.
-        TODO: Best case running time: ??? under what conditions?
-        TODO: Worst case running time: ??? under what conditions?"""
+        Best case running time: O(1) if the root is what we are finding
+        Worst case running time: O(log(n)) if tree is ordered and balanced we have essential
+                                        cut it in halve with each iteration"""
         # Find a node with the given item, if any
         node = self._find_node_recursive(item, self.root)
         # Return True if a node was found, or False
@@ -80,36 +83,38 @@ class BinarySearchTree(object):
     def search(self, item):
         """Return an item in this binary search tree matching the given item,
         or None if the given item is not found.
-        TODO: Best case running time: ??? under what conditions?
-        TODO: Worst case running time: ??? under what conditions?"""
+        Best case running time: O(1) if the root is what we are finding
+        Worst case running time: O(log(n)) if tree is ordered and balanced we have essential
+                                    cut it in halve with each iteration"""
         # Find a node with the given item, if any
         node = self._find_node_recursive(item, self.root)
-        # TODO: Return the node's data if found, or None
-        return node.data if ... else None
+        # Return the node's data if found, or None
+        return node.data if node is not None else None
 
     def insert(self, item):
         """Insert the given item in order into this binary search tree.
-        TODO: Best case running time: ??? under what conditions?
-        TODO: Worst case running time: ??? under what conditions?"""
+        Best case running time: O(1) if the root is what we are finding
+        Worst case running time: O(log(n)) if tree is ordered and balanced we have essential
+                                    cut it in halve with each iteration"""
         # Handle the case where the tree is empty
         if self.is_empty():
-            # TODO: Create a new root node
+            # Create a new root node
             self.root = BinaryTreeNode(item)
-            # TODO: Increase the tree size
-            self.size ...
-            return
+            # Increase the tree size
+            self.size += 1
+            return None
         # Find the parent node of where the given item should be inserted
-        parent = self._find_parent_node_recursive(item, self.root)
-        # TODO: Check if the given item should be inserted left of parent node
-        if ...:
-            # TODO: Create a new node and set the parent's left child
-            parent.left = ...
-        # TODO: Check if the given item should be inserted right of parent node
-        elif ...:
-            # TODO: Create a new node and set the parent's right child
-            parent.right = ...
-        # TODO: Increase the tree size
-        self.size ...
+        parent = self._find_parent_node_iterative(item)
+        # Check if the given item should be inserted left of parent node
+        if item < parent.data:
+            # Create a new node and set the parent's left child
+            parent.left = BinaryTreeNode(item)
+        # Check if the given item should be inserted right of parent node
+        elif item > parent.data:
+            # Create a new node and set the parent's right child
+            parent.right = BinaryTreeNode(item)
+        # Increase the tree size
+        self.size += 1
 
     def _find_node_iterative(self, item):
         """Return the node containing the given item in this binary search tree,
